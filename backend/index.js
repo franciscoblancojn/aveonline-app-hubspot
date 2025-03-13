165ccf564822d1d5eabf1d90ddd97417862fcce3
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-13 16:52:54 -0500

    log

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const request = require("request-promise-native");
 const NodeCache = require("node-cache");
 const session = require("express-session");
 const app = express();
-const NodeCache = require("node-cache");
 
 const PORT = 3000;
 
