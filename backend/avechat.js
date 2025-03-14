890c156bb02d626a7d37c32e7c5b54af0c5a386f
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-14 11:03:11 -0500

    add postUrlHs

--- backend/avechat.js
+++ backend/avechat.js
@@ -23,21 +23,33 @@ class AveChat {
         }
     }
     
-    async getIdHs(){
+    async getIdCustomFiled(){
         const result = await this.onRequest({
             url:"/accounts/custom_fields"
         })
         const id_hs = result.find(e=>e.name=="id_hs").id
+        const url_hs = result.find(e=>e.name=="url_hs").id
 
-        return id_hs
+        return {id_hs,url_hs}
     }
     async postIdHs({user_id, id_hs}){
         const IDHS = await this.getIdHs()
         const result = await this.onRequest({
-            url:`/users/${user_id}/custom_fields/${IDHS}`,
+            url:`/users/${user_id}/custom_fields/${IDHS.id_hs}`,
             method:"POST",
             body:new URLSearchParams({
                 'value': id_hs
+            })
+        })
+        return result;
+    }
+    async postUrlHs({user_id, url_hs}){
+        const IDHS = await this.getIdHs()
+        const result = await this.onRequest({
+            url:`/users/${user_id}/custom_fields/${IDHS.url_hs}`,
+            method:"POST",
+            body:new URLSearchParams({
+                'value': url_hs
             })
         })
         return result;
