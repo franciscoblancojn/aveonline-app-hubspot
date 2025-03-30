c8ebf4fe413b47e849441638e433906e3e91c73b
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-30 14:02:35 -0500

    test

--- backend/file.js
+++ backend/file.js
@@ -3,11 +3,12 @@ const fs = require('fs').promises;
 
 const FILE_PATH = 'data.json';
 class File {
-
+    n_asesor_comercial = 0
     constructor(){
         
     }
     async getStoredNumber(){
+        return this.n_asesor_comercial
         try {
             const data = await fs.readFile(FILE_PATH, 'utf8');
             return JSON.parse(data).n_asesor_comercial || 0;
@@ -18,6 +19,8 @@ class File {
     
     // Función para guardar el número en el archivo
     async saveNumber(n_asesor_comercial){
+        this.n_asesor_comercial = n_asesor_comercial
+        return
         await fs.writeFile(FILE_PATH, JSON.stringify({ n_asesor_comercial }));
     };
 }
