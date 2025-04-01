3876bd2568db42299870143a6e6a0df996c4d13b
franciscoblancojn <blancofrancisco34@gmail.com>
2025-04-01 08:13:18 -0500

    test

--- backend/index.js
+++ backend/index.js
@@ -812,15 +812,27 @@ app.post("/api/ave-chat/validate-date", async (req, res) => {
 });
 app.post("/api/hubspot/create-company", async (req, res) => {
   try {
-    const result = await hubspot.crearCompany({
-      id_hs:req?.body?.id_hs,
-      name:req?.body?.name,
-      phone:req?.body?.phone,
-    })
+    let associationTypeId =1
+    let sw = true
+    do {
+      accessTokenCache.set("associationTypeId", associationTypeId);
+      const result = await hubspot.crearCompany({
+        id_hs:req?.body?.id_hs,
+        name:req?.body?.name,
+        phone:req?.body?.phone,
+        associationTypeId
+      })
+      if(result.status == "error"){
+        associationTypeId++
+      }else{
+        sw = false
+      }
+    } while (sw);
     return res.json({
       success: true,
       message: "✅ Company creado correctamente.",
-      result
+      result,
+      associationTypeId
     });
   } catch (error) {
     accessTokenCache.set("create-contact-error", error);
