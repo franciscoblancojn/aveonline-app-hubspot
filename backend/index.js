4947b4d7fe47d6dc3b4d0b1e895cdaee077d855f
franciscoblancojn <blancofrancisco34@gmail.com>
2025-06-05 18:07:16 -0500

    /api/form-campana/ave-chat/create-contact

--- backend/index.js
+++ backend/index.js
@@ -1244,10 +1244,10 @@ app.post("/api/form-campana/ave-chat/create-contact", async (req, res) => {
     // });
   try {
     const data = {
-      id_avechat: "57"+req?.body?.["fields[phone][value]"]?.replace(/\D/g, ""),
-      name: req?.body?.["fields[name][value]"],
-      phone: req?.body?.["fields[phone][value]"].replace(/\D/g, ""),
-      campana: req?.body?.["fields[campana][value]"],
+      id_avechat: "57"+req?.body?.fields?.phone?.value?.replace(/\D/g, ""),
+      name: req?.body?.fields?.name?.value,
+      phone: req?.body?.fields?.phone?.value?.replace(/\D/g, ""),
+      campana: req?.body?.fields?.campana?.value,
     };
     const userAveChat = await aveChat.createUserIfNotExist({
       id_avechat: data.id_avechat,
