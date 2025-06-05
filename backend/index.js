30fad22e4e59e5fab361183553369f976a6ef3db
franciscoblancojn <blancofrancisco34@gmail.com>
2025-06-05 18:05:07 -0500

    /api/form-campana/ave-chat/create-contact

--- backend/index.js
+++ backend/index.js
@@ -73,6 +73,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 //================================//
 //   Running the OAuth 2.0 Flow   //
 //================================//
